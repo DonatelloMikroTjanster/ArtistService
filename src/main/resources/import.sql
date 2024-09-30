15fb0bd3d44035bcc6e33e5e01c495a9ea0ed156
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2024-09-30 19:37:01 +0200

    Lots of changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,22 +1,18 @@
 -- Insert Artists
-INSERT INTO artist (name) VALUES ('The Beatles');
-INSERT INTO artist (name) VALUES ('Michael Jackson');
-INSERT INTO artist (name) VALUES ('Elon Musk');
-INSERT INTO artist (name) VALUES ('David Attenborough');
-INSERT INTO artist (name) VALUES ('Joe Rogan');
+INSERT INTO artist (id, name, genre)
+VALUES
+    (1, 'The Beatles', 'Rock'),
+    (2, 'Michael Jackson', 'Pop');
 
--- Insert Albums
-INSERT INTO album (name, release_date) VALUES ('Abbey Road', '1969-09-26');
-INSERT INTO album (name, release_date) VALUES ('Revolver', '1966-08-05');
-INSERT INTO album (name, release_date) VALUES ('Thriller', '1982-11-30');
-INSERT INTO album (name, release_date) VALUES ('Tech Talk Series', '2023-01-01');
-INSERT INTO album (name, release_date) VALUES ('Planet Earth II', '2016-11-06');
+-- Insert Tracks
+INSERT INTO track (id, title, release_date, album_id)
+VALUES
+    (1, 'Come Together', '1969-09-26', 1),
+    (2, 'Billie Jean', '1982-11-30', 2);
 
---Insert albums_artist
-INSERT INTO album_artist (album_id, artist_id) VALUES (1,1);
-INSERT INTO album_artist (album_id, artist_id) VALUES (2,1);
-INSERT INTO album_artist (album_id, artist_id) VALUES (3,2);
-INSERT INTO album_artist (album_id, artist_id) VALUES (4,3);
-INSERT INTO album_artist (album_id, artist_id) VALUES (5,4);
-INSERT INTO album_artist (album_id, artist_id) VALUES (4,5);
 
+-- Insert Album-Artist relationships
+INSERT INTO album_artist (album_id, artist_id)
+VALUES
+    (1, 1),  -- Abbey Road - The Beatles
+    (2, 2);  -- Thriller - Michael Jackson
